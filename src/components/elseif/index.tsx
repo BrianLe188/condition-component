0cd6b322cee3302482cbf743db20f86b17652132
brian.h <anhkun123456@gmail.com>
2024-09-15 16:31:44 +0700

    fix: cannot be used as a JSX component.      Its return type 'ReactNode' is not a valid JSX element.        Type 'undefined' is not assignable to type 'Element | null'

--- src/components/elseif/index.tsx
+++ src/components/elseif/index.tsx
@@ -3,5 +3,5 @@ import { PropsWithChildren } from "react";
 export type ElseIfProps = { condition: boolean } & PropsWithChildren;
 
 export default function ElseIf({ condition, children }: ElseIfProps) {
-  return condition && children;
+  return condition ? children : null;
 }
--- src/components/if/index.tsx
+++ src/components/if/index.tsx
@@ -3,5 +3,5 @@ import { PropsWithChildren } from "react";
 export type IfProps = { condition: boolean } & PropsWithChildren;
 
 export default function If({ condition, children }: IfProps) {
-  return condition && children;
+  return condition ? children : null;
 }
