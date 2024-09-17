82fb22c3c7b24b0f7aec66b7c444c502c790dc9f
brian.h <anhkun123456@gmail.com>
2024-09-17 20:30:35 +0700

    fix #1: default handler missing in switch statement

---
+++ src/components/default/index.tsx
@@ -0,0 +1,5 @@
+import { WithChildren } from "@/utils/type";
+
+export default function Default({ children }: WithChildren) {
+  return <>{children}</>;
+}
--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -2,25 +2,39 @@
 import { Children, isValidElement } from "react";
 import Case from "../case";
 import { WithChildren } from "@/utils/type";
+import Default from "../default";
 
 export type SwitchProps = {
   value: unknown;
 } & WithChildren;
 
 export default function Switch({ value, children }: SwitchProps): any {
-  const cases = Children.toArray(children).reduce((prev, child) => {
-    if (
-      isValidElement(child) &&
-      typeof child.type !== "string" &&
-      child.type.name === Case.name
-    ) {
-      return {
-        ...prev,
-        [child.props.value]: child.props.children,
-      };
-    }
-    return prev;
-  }, {});
+  const cases = Children.toArray(children).reduce(
+    (prev, child) => {
+      if (isValidElement(child) && typeof child.type !== "string") {
+        if (child.type.name === Default.name && prev?.[Default.name]) {
+          throw new Error(
+            "Uncaught SyntaxError: More than one default clause in switch statement",
+          );
+        }
+        switch (child.type.name) {
+          case Case.name:
+            return {
+              ...prev,
+              [child.props.value]: child.props.children,
+            };
+          default:
+          case Default.name:
+            return {
+              ...prev,
+              [Default.name]: child.props.children,
+            };
+        }
+      }
+      return prev;
+    },
+    {} as Record<string, any>,
+  );
 
   return <>{cases?.[value as keyof typeof cases] || null}</>;
 }
